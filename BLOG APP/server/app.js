0cb9452b38e158e70e5c845680a1f558961567eb
YASH VARDHAN PRASAD <yashvardhanprasad@YASHs-MacBook-Pro.local>
2020-04-05 19:57:06 +0530

    first commit

---
+++ BLOG APP/server/app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const fs = require('fs');
+dotenv.config();
+
+// db
+// MONGO_URI=mongodb://localhost/nodeapi
+
+mongoose.connect('mongodb://127.0.0.1:27017/blog-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+  .then(() => console.log("DB Connected"));
+
+  mongoose.connection.on("error", err => {
+      console.log(`DB connection error: ${err.message}`);
+  });
+
+// bring in routes
+const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+
+app.get('/',(req, res) => {
+
+     fs.readFile('docs/apiDocs.json', (err, data) => {
+       if(err){
+         res.status(400).json({
+            error: err
+         })
+       }
+       const docs = JSON.parse(data);
+       res.json(docs);
+     })
+})
+// middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+app.use("/", postRoutes);
+app.use("/", authRoutes);
+app.use("/", userRoutes);
+
+app.use(function(err, res, req, next) {
+  if (err.name === 'UnauthorizedError'){
+    res.status(401).json({error: "Unauthorized"})
+  }
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log('Server started @ ' + port)
+})
